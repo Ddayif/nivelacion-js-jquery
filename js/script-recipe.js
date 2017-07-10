9f957c28c0d5098d5f055cbc8ff7aab565cc8992
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-10 17:25:09 -0300

    Aca comienza la Etapa 7 donde se imprime un mensaje en la consola con el archivo recipe.html

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	$(".js-menu").hide(); // hide ocultara la clase js-menu
 
 	$(".js-back").show();// show mostrara la clase anteriormente ocultada en el archivo script-index.js (Flecha)
+
+	console.log("Hola Mundo");
 })
 	
 	
