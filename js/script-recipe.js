3d90ae280d38f2ee518ca0264324f1761b7c480d
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-10 11:18:13 -0300

    Esta aun es la primera estapa, aca se mejora el ocultar y a su vez mostrar los elementos correspondientes

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-	$(".js-menu");
+	$(".js-menu").hide(); // hide ocultara la clase js-menu
 })
+	
+	$(".js-back").show();// show mostrara la clase anteriormente ocultada en el archivo script-index.js (Flecha)
