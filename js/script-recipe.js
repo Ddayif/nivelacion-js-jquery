5b9c197ee7d8e04bbc6494375b0fdac1471b8fce
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-10 22:56:00 -0300

    Aca comienza la Etapa 8, se asigna funcion para cuando el usuario de click al boton con clase js-show-recipe y el boton con clase js-show-make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
 	$(".js-back").show();// show mostrara la clase anteriormente ocultada en el archivo script-index.js (Flecha)
 
 	console.log("Hola Mundo");
+
+	$(".js-show-recipe").click(function(){
+		console.log("funciona")
+	$(".js-show-recipe").append("<div><p class=saludo>Botón Recipe</p></div>")
+	})
+
+	$(".js-show-make").click(function(){
+		console.log("funciona")
+	$(".js-show-make").append("<div><p class=saludo>Botón Make</p></div>")
+	})
+
+	$("div.page").click(function(){
+		$("js-show-make").append("make");
+		console.log("funciona");
+	});
 })
 	
 	
